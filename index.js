b0270737b145e074b1bcb27de72be1481f396c97
cristians775 <cristiansaldivia775@gmail.com>
2020-03-10 20:46:42 -0300

    superagent

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-const superagent = require('superagent');
-
+const superagent = require("superagent");
+const fs = require("fs");
 function imprimirMuseos(error, respuesta) {
   if (error) {
-    throw new Error('algo se rompió', error);
+    throw new Error("algo se rompió", error);
   }
 
   const cantidad = respuesta.body.count;
   const museos = respuesta.body.results;
 
   console.log(`Se encontraron ${cantidad} museos.`);
-  console.log(`El primer museo se llama ${museos[0].nombre}.`)
+  console.log(`El primer museo se llama ${museos[0].nombre}.`);
 }
 
-console.log('Antes de llamar a superagent')
+console.log("Antes de llamar a superagent");
 
 superagent
-  .get('https://www.cultura.gob.ar/api/v2.0/museos')
-  .query({ format: 'json' })
-  .end(imprimirMuseos)
+  .get("https://www.cultura.gob.ar/api/v2.0/museos")
+  .query({ format: "json" })
+  .end((err, res) => {
+    const nombres = res.body.results.map(e =>`${e.nombre}. Por cualquier consulta comunicarse al ${e.telefono}`);
+   console.log(nombres)
+    fs.writeFile('museos.txt',nombres.join('\n'),(e)=>{});
+   
+  });
 
-console.log('Después de llamar a superagent')
+console.log("Después de llamar a superagent");
+
+
+
